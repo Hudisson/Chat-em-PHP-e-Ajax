720b56a30694b4fe26d3430f083cdd92a884d997
Hudisson R Xavier <hudissonxavier2018@gmail.com>
2025-11-18 15:46:57 -0300

    Desenvolvido Layout do chat

--- resources/views/home.php
+++ resources/views/home.php
@@ -2,93 +2,104 @@
 include_once "includes/header.php";
 ?>
 
-
 <div class="container wrapper">
-    <section class="users">
-        <header>
+    <header>
+        <div class="header-info">
 
-            <div class="content">
-
-                <div class="logo-chat">
-                    <h1>Home Chat</h1>
-                </div>
-
-                <div class="img-foto">
-                    <img src="../../../chat/resources/assets/img/h.jpeg" alt="">
-                </div>
-
-
-                <div class="details">
-                    <span>João</span>
-                    <p>Oline</p>
-                    <a href="" class="logout">Sair</a>
-                </div>
-
+            <div class="logo-chat">
+                <h1>Home Chat</h1>
             </div>
 
-        </header>
-    </section>
-
-    <main>
-        <div class="container main">
-
-            <div class="chat-view">
-                <div class="contact-msg">
-                    <span class="contact-name">Fulano da Silva</span> <span class="status">Oline</span>
-                </div>
-
-                <hr>
-
-                <div class="conversation">
-
-                    <div class="msg-received">
-                        Olá!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Perferendis nihil ullam 
-                    </div>
-
-                    <div class="msg-sent">
-                        Oi!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Perferendis nihil ullam
-                    </div>
-                </div>
-
+            <div class="profile-summary">
+                <span>João</span>
+                <span class="online-status">Online</span>
+                <a href="#" class="logout">Sair</a>
+            </div>
+            
+            <div class="profile-image-container">
+                <img src="../../../chat/resources/assets/img/h.jpeg" alt="Foto de Perfil de João">
             </div>
 
-            <div class="contact-list">
-
-                <div class="search">
-                    <label>Pesquisar contato</label>
-                    <input type="text" class="form-control pesquisar" id="search" placeholder="Pesquisar">
-                </div>
-
-                <hr>
-
-                <div class="friends">
-                    <img src="../../../chat/resources/assets/img/h.jpeg" alt="">
-                    <span class="friend-name">Fulano da Silva</span>
-                    <span class="status">Oline</span>
-                </div>
-
-                <div class="friends">
-                    <img src="../../../chat/resources/assets/img/m.jpeg" alt="">
-                    <span class="friend-name">Beltrano dos Santos</span>
-                    <span class="status">Oline</span>
-                </div>
-
-                <div class="friends">
-                    <img src="../../../chat/resources/assets/img/m.jpeg" alt="">
-                    <span class="friend-name">Ciclano de Tal</span>
-                    <span class="status">Oline</span>
-                </div>
-
-            </div>
         </div>
+    </header>
 
-    </main>
+    <div class="container main-layout">
+        
+        <div class="chat-view">
+            <div class="contact-msg">
+                <span class="contact-name">Fulano da Silva</span>
+                <span class="status">Online</span>
+            </div>
 
+            <div class="conversation">
+                
+                <div class="msg-received">
+                    Olá!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Perferendis nihil ullam 
+                </div>
 
-    <?php
+                <div class="msg-sent">
+                    Oi!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Perferendis nihil ullam
+                </div>
+                 <div class="msg-received">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil.
+                </div>
+
+                <div class="msg-sent">
+                    Consectetur adipisicing elit. 
+                </div>
+
+                <div class="msg-received">
+                    Blábla bla Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil.
+                </div>
+
+                <div class="msg-sent">
+                    Consectetur adipisicing elit. 
+                </div>
+
+            </div>
+
+            </div>
+
+        <aside class="contact-list"> <div class="search">
+                <label for="search-input">Pesquisar contato</label>
+                <input type="text" class="form-control" id="search-input" placeholder="Pesquisar">
+            </div>
+
+            <div class="contacts-container">
+                <div class="friend-item">
+                    <img src="../../../chat/resources/assets/img/h.jpeg" alt="Foto de Perfil">
+                    <div class="friend-info">
+                        <span class="friend-name">Fulano da Silva</span>
+                    </div>
+                    <span class="status">Online</span>
+                </div>
+
+                <div class="friend-item">
+                    <img src="../../../chat/resources/assets/img/m.jpeg" alt="Foto de Perfil">
+                    <div class="friend-info">
+                        <span class="friend-name">Beltrano dos Santos</span>
+                    </div>
+                    <span class="status">Online</span>
+                </div>
+
+                <div class="friend-item">
+                    <img src="../../../chat/resources/assets/img/m.jpeg" alt="Foto de Perfil">
+                    <div class="friend-info">
+                        <span class="friend-name">Ciclano de Tal</span>
+                    </div>
+                    <span class="status">Online</span>
+                </div>
+            </div>
+
+        </aside>
+    </div>
+
+</div>
+
+<?php
     include_once "includes/footer.php";
-    ?>
+?>
